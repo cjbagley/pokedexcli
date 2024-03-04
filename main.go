205170601071f171e43fc6cea027eb32066f9e00
Colin Bagley <cjbagley@googlemail.com>
2024-03-04 20:37:49 +0000

    wip: Accepting user input

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/cjbagley/pokedexcli/cmds"
 )
 
+const Prompt = "Pokedex > "
+
 func main() {
-	fmt.Print("Hello, world!")
+	cmds := cmds.GetCliCommands()
+	scanner := bufio.NewScanner(os.Stdin)
+	r, _ := regexp.Compile("[^A-Za-z]")
+
+	fmt.Print(Prompt)
+	for scanner.Scan() {
+		input := r.ReplaceAllString(scanner.Text(), "")
+
+		fmt.Println(input)
+
+		cmd, ok := cmds[input]
+		if ok {
+			cmd.Callback()
+		}
+		fmt.Print(Prompt)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
